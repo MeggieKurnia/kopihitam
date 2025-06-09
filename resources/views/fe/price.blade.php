f6efbdf56b92d334b44f453cc2d79b48a568f6c4
meggi <meggi@webarq.com>
2025-06-09 23:35:02 +0700

    ds

--- resources/views/fe/price.blade.php
+++ resources/views/fe/price.blade.php
@@ -1,53 +1,63 @@
-@if(count($data))
-<section id="pricing" class="pricing section">
+  @if(count($data))
+  <section id="pricing" class="pricing section">
+    <style type="text/css">
+      
+      p.judul-section::after{
+            content: "";
+          width: 120px;
+          height: 1px;
+          display: inline-block;
+          background: var(--accent-color);
+          margin: 4px 10px;
+      }     
+    </style>
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
+      <p class="judul-section ungu">Paket {{$label}}</p>
+    </div><!-- End Section Title -->
 
-  <!-- Section Title -->
-  <div class="container section-title" data-aos="fade-up">
-    <h2 class="judul-section ungu">Paket {{$label}}</h2>
-  </div><!-- End Section Title -->
+    <div class="container">
 
-  <div class="container">
-
-    <div class="row gy-3 justify-content-center">
-      @foreach($data as $p)
-        @php $p = (object)$p; @endphp
-      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
-        <div class="pricing-item @if($p->is_unggulan) featured @endif">
-          @if($p->is_advance)
-            <span class="advanced">Advanced</span>
-          @endif
-          <h3>{{$p->title}}</h3>
-          @if($p->harga_coret)
-          <h6 class="price-strike">
-            {!! $p->harga_coret !!}
-          </h6>
-          @endif
-          <h4>{!! $p->harga !!}</h4>
-          @php
-            $w = widget('price','item',$p->id);
-            if(count($w)){
-          @endphp
-          <ul>
-            @foreach($w as $l)
-                <li class="abu-abu">{{$l['items']}}</li>
-            @endforeach
-          </ul>
-          @php } @endphp
-          <div class="btn-wrap">
-            <a href="{{$p->link_buy}}" target="_blank" class="btn-buy">{{\App\Helper::getSetting('localization','buy_now')}}</a>
+      <div class="row gy-3 justify-content-center">
+        @foreach($data as $p)
+          @php $p = (object)$p; @endphp
+        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+          <div class="pricing-item @if($p->is_unggulan) featured @endif">
+            @if($p->is_advance)
+              <span class="advanced">Advanced</span>
+            @endif
+            <h3>{{$p->title}}</h3>
+            @if($p->harga_coret)
+            <h6 class="price-strike">
+              {!! $p->harga_coret !!}
+            </h6>
+            @endif
+            <h4>{!! $p->harga !!}</h4>
+            @php
+              $w = widget('price','item',$p->id);
+              if(count($w)){
+            @endphp
+            <ul>
+              @foreach($w as $l)
+                  <li class="abu-abu">{{$l['items']}}</li>
+              @endforeach
+            </ul>
+            @php } @endphp
+            <div class="btn-wrap">
+              <a href="{{$p->link_buy}}" target="_blank" class="btn-buy">{{\App\Helper::getSetting('localization','buy_now')}}</a>
+            </div>
           </div>
-        </div>
-      </div><!-- End Pricing Item -->
-      @endforeach
+        </div><!-- End Pricing Item -->
+        @endforeach
+
+      </div>
 
     </div>
 
-  </div>
-
-</section>
-@endif
-@if(isset($home))
-  @if($start)
-    @include('fe.started')
+  </section>
   @endif
-@endif
+  @if(isset($home))
+    @if($start)
+      @include('fe.started')
+    @endif
+  @endif
