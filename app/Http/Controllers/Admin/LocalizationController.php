c31ccb02bf27cd6027798cccaace87973f05887d
meggi <meggi@webarq.com>
2025-06-15 23:52:52 +0700

    update

--- app/Http/Controllers/Admin/LocalizationController.php
+++ app/Http/Controllers/Admin/LocalizationController.php
@@ -28,7 +28,63 @@ class LocalizationController extends CRUDController{
 				'type'=>'text',
 				'label'=>'Faq',
 				'value'=>Helper::getSetting('localization','faq')
-			]
+			],
+			'feture'=>[
+				'type'=>'text',
+				'label'=>'Feature',
+				'value'=>Helper::getSetting('localization','feture')
+			],
+			'check_feature'=>[
+				'type'=>'text',
+				'label'=>'Check Feature',
+				'value'=>Helper::getSetting('localization','check_feature')
+			],
+			'why_we'=>[
+				'type'=>'text',
+				'label'=>'Why We',
+				'value'=>Helper::getSetting('localization','why_we')
+			],
+			'why_we_title'=>[
+				'type'=>'text',
+				'label'=>'Why We Title',
+				'value'=>Helper::getSetting('localization','why_we_title')
+			],
+			'client'=>[
+				'type'=>'text',
+				'label'=>'Client',
+				'value'=>Helper::getSetting('localization','clinet')
+			],
+			'testimonial'=>[
+				'type'=>'text',
+				'label'=>'Testimonial',
+				'value'=>Helper::getSetting('localization','testimonial')
+			],
+			'project_info'=>[
+				'type'=>'text',
+				'label'=>'Project Information',
+				'value'=>Helper::getSetting('localization','project_info')
+			],
+			'project_cat'=>[
+				'type'=>'text',
+				'label'=>'Project Category',
+				'value'=>Helper::getSetting('localization','project_cat')
+			],
+			'project_cln'=>[
+				'type'=>'text',
+				'label'=>'Project Clients',
+				'value'=>Helper::getSetting('localization','project_cln')
+			],
+			'project_date'=>[
+				'type'=>'text',
+				'label'=>'Project Date',
+				'value'=>Helper::getSetting('localization','project_date')
+			],
+			'project_url'=>[
+				'type'=>'text',
+				'label'=>'Project URL',
+				'value'=>Helper::getSetting('localization','project_url')
+			],
+
 		];
 	}
 }
