558b58b8efc3186902d4a2dabe8ccf7ce5111bb4
meggi <meggi@webarq.com>
2025-06-16 01:13:00 +0700

    update

---
+++ database/migrations/2025_06_15_180105_alter_menus.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->string('badge_text')->nullable();
+            $table->integer('badge_new_line')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->dropColumn('badge_text');
+            $table->dropColumn('badge_new_line');
+        });
+    }
+};
